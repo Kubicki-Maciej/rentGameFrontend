af43589a3eb0934a782dbf9534af374c2fb29011
Maciej Kubicki <dev.kubicki@gmail.com>
2023-08-20 20:29:42 +0200

    changed api server to vercel

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ axios.defaults.xsrfHeaderName = "X-CSRFToken";
 axios.defaults.withCredentials = true;
 
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://rent-game.vercel.app/",
 });
 
 function App() {
